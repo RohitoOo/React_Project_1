ea654ee4532433f94142362a718ac8179c210178
RohitoOo <bhambhani.ba@gmail.com>
2018-05-17 09:19:17 +0530

    Initial Commit

---
+++ src/Another_App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Another_App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome Back to React</h1>
+          {props.}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Another_App;
