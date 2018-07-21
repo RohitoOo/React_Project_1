380e0b64dfdff4909cfe3e7079f754c7a72be269
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-21 16:35:31 +0530

    Edited map function

--- src/Me.js
+++ src/Me.js
@@ -4,10 +4,10 @@ import './App.css';
 function Me({data}) {
 
   return (<ul>
-    {data.details.map(function(x){
+    {data.details.map(function(data){
       return <li className="list_items">
- {x.username}  { x.provider} || {x.engagement}  {x.channel}
- {x.url } {x.link } {x.type }</li>
+ {data.username}  { data.provider} || {data.engagement}  {data.channel}
+ {data.url } {data.link } {data.type }</li>
     })}
     </ul>
     )
